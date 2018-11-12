798ab1d977d4915b1fe579c9955bfda59fc34fc5
Cedric Dlh <cedricdlh@gmail.com>
2018-11-12 14:36:14 +0100

    Created a stupid coach app

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I'm going to work"
+      return @response = "Great!"
+    elsif @question.include? "?"
+      return @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work"
+    end
+  end
+end
